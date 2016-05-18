b2e2f2ba21ff643f3a55f868bd7ee1ae162607d5
tobby88 <spam@tobby.eu>
2016-05-18 02:11:01 +0200

    Set version string and version/build numbers

--- Max/Properties/AssemblyInfo.cs
+++ Max/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("Max")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Max!")]
+[assembly: AssemblyDescription("Max! application for Helmut")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Max")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Tobias Holst")]
+[assembly: AssemblyProduct("Max!")]
+[assembly: AssemblyCopyright("Copyright ©  2015-today Tobias Holst")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+//[assembly: AssemblyFileVersion("2.0.*")]
